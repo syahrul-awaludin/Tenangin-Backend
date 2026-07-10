60fbe40169d8a76e71aa303b2fcb9ab78b2eaf4f
4rthemis <syahrul.uix@gmail.com>
2026-07-10 10:49:23 +0700

    fix: allow process.env.PRISMA_CLIENT_ENGINE_TYPE to reach PrismaClient

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -3,6 +3,7 @@ const { PrismaClient } = require('@prisma/client');
 
 // Singleton: satu instance untuk seluruh aplikasi
 // Mencegah terlalu banyak koneksi ke MySQL
+
 const prisma = new PrismaClient({
   log: process.env.NODE_ENV === 'development'
     ? ['query', 'info', 'warn', 'error']
